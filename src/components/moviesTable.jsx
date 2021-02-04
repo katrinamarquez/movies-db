19a2967515f0f3dc5a5930a205559e20fab4d062
katrinamarquez <katrina.l.marquez@gmail.com>
2021-02-04 14:00:37 +1000

    table header component

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Like from './common/like';
+import TableHeader from './common/tableHeader';
 
-const MoviesTable = props => {
-  const { movies, onDelete, onLike, onSort } = props;
+class MoviesTable extends Component {
+  columns = [
+    { path: 'title', label: 'Title' },
+    { path: 'genre.name', label: 'Genre' },
+    { path: 'numberInStock', label: 'Stock' },
+    { path: 'dailyRentalRate', label: 'Rate' },
+    { key: 'like' },
+    { key: 'delete' }
+  ];
+
+  render() { 
+    const { movies, onDelete, onLike, onSort, sortColumn } = this.props;
 
   return ( 
     <table className="table">
-      <thread>
-        <tr>
-          <th onClick={() => onSort("title")}>Title</th>
-          <th onClick={() => onSort("genre.name")}>Genre</th>
-          <th onClick={() => onSort("numberInStock")}>Stock</th>
-          <th onClick={() => onSort("dailyRentalRate")}>Rate</th>
-          <th />
-          <th />
-        </tr>
-      </thread>
+      <TableHeader 
+        columns={this.columns} 
+        sortColumn={sortColumn} 
+        onSort={onSort} 
+      />
       <tbody>
         {movies.map(movie => (
           <tr key={movie._id}>
@@ -42,6 +48,7 @@ const MoviesTable = props => {
       </tbody>
   </table>
   );
+  }
 }
  
 export default MoviesTable;
