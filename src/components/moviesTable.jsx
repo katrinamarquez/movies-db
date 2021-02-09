dd45c47ebd5eead4eaeae9c5ac6db26b4753d167
katrinamarquez <katrina.l.marquez@gmail.com>
2021-02-09 16:00:59 +1000

    add in begining of movies form

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Table from './common/table';
 import Like from './common/like';
 class MoviesTable extends Component {
   columns = [
-    { path: 'title', label: 'Title' },
+    { 
+      path: 'title', 
+      label: 'Title', 
+      // to is a template literal
+      content: movie => <Link to={`/movies/${movie._id}`}>{movie.title}</Link> 
+    },
     { path: 'genre.name', label: 'Genre' },
     { path: 'numberInStock', label: 'Stock' },
     { path: 'dailyRentalRate', label: 'Rate' },
