16c82cdb8218f1a32e469a3ac2ef7885b9485b99
katrinamarquez <katrina.l.marquez@gmail.com>
2021-02-10 15:57:36 +1000

    login input working

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class LoginForm extends Component {
+  state = {
+    account: { username: '', password: '' }
+  };
+
   handleSubmit = e => {
     // this prevents submitting a form to the server which causes a full page reload
     e.preventDefault();
@@ -10,18 +14,41 @@ class LoginForm extends Component {
     console.log('submitted');
   };
 
-  render() { 
+  handleChange = ({ currentTarget: input }) => {
+    // Do not want to update state directly. We want to clone it and then have react update the state
+    const account = {...this.state.account}
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
+  render() {
+    const { account } = this.state;
+    
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
               <label htmlFor="username">Username</label>
-              <input id="username" type="text" className="form-control"/>
+              <input
+                value={account.username} 
+                onChange={this.handleChange}
+                id="username" 
+                name="username"
+                type="text" 
+                className="form-control"
+              />
             </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control"/>
+            <input 
+              value={account.password}
+              onChange={this.handleChange}
+              id="password" 
+              name="password"
+              type="text" 
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
