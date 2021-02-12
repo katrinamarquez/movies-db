36db3230157fcaa8432ab1c6ccadbc539318ef0a
katrinamarquez <katrina.l.marquez@gmail.com>
2021-02-12 11:40:52 +1000

    login form errors working

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -3,13 +3,32 @@ import Input from './common/input';
 
 class LoginForm extends Component {
   state = {
-    account: { username: '', password: '' }
+    account: { username: '', password: '' },
+    // Properties in this object map to the properties of the input field
+    errors: {}
+  };
+
+  validate = () => {
+    const errors = {};
+
+    const { account } = this.state; 
+    if (account.username.trim() === '')
+      errors.username = 'Username is required.';
+    if (account.password.trim() === '')
+      errors.password = 'Password is required.';
+
+      return Object.keys(errors).length === 0 ? null : errors; 
   };
 
   handleSubmit = e => {
     // this prevents submitting a form to the server which causes a full page reload
     e.preventDefault();
 
+    const errors = this.validate();
+    console.log(errors);
+    // Updates state which will cause re-rendering and then show error message
+    this.setState({ errors });
+    if (errors) return;
     // call the server, save changes, then redirect user to new page
     // this returns the actual DOM element
     console.log('submitted');
