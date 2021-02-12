3953dc6ade248e1f702a26fe8545344225ea47e8
katrinamarquez <katrina.l.marquez@gmail.com>
2021-02-12 14:11:29 +1000

    validation update so far

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Input from './common/input';
+import Joi from 'joi-browser';
 
 class LoginForm extends Component {
   state = {
@@ -8,16 +9,30 @@ class LoginForm extends Component {
     errors: {}
   };
 
+  schema = {
+    username: Joi.string()
+      .required()
+      .label('Username'),
+    password: Joi.string()
+      .required()
+      .label('Password')
+  };
+
   validate = () => {
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+    if (!error) return null; 
+
     const errors = {};
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
+  };
 
-    const { account } = this.state; 
-    if (account.username.trim() === '')
-      errors.username = 'Username is required.';
-    if (account.password.trim() === '')
-      errors.password = 'Password is required.';
-
-      return Object.keys(errors).length === 0 ? null : errors; 
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    Joi.validate(obj, schema);
+    return error ? error.details[0].message : null; 
   };
 
   handleSubmit = e => {
@@ -27,7 +42,7 @@ class LoginForm extends Component {
     const errors = this.validate();
     console.log(errors);
     // Updates state which will cause re-rendering and then show error message
-    this.setState({ errors });
+    this.setState({ errors: errors || {}  });
     if (errors) return;
     // call the server, save changes, then redirect user to new page
     // this returns the actual DOM element
@@ -35,14 +50,19 @@ class LoginForm extends Component {
   };
 
   handleChange = ({ currentTarget: input }) => {
+    const errors = {...this.state.errors};
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     // Do not want to update state directly. We want to clone it and then have react update the state
     const account = {...this.state.account}
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
     
     return (
       <div>
@@ -53,12 +73,14 @@ class LoginForm extends Component {
             value={account.username} 
             label="Username" 
             onChange={this.handleChange} 
+            error={errors.username}
           />
           <Input 
               name="password" 
               value={account.password} 
               label="Password" 
               onChange={this.handleChange} 
+              error={errors.password}
             />
           <button className="btn btn-primary">Login</button>
         </form>
